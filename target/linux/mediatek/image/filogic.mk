7c8e0001e1997a90af04f1f2ba96dc96b02a081b
yoyo <xiaoxie280@163.com>
2026-03-26 13:09:39 +0000

    add packages for hy3000
    Signed-off-by: yoyo <xiaoxie280@163.com>

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -963,7 +963,7 @@ define Device/philips_hy3000
   DEVICE_MODEL := HY3000
   DEVICE_DTS := mt7981b-philips-hy3000
   DEVICE_DTS_DIR := ../dts
-  DEVICE_PACKAGES := f2fsck mkf2fs
+  DEVICE_PACKAGES := kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware automount f2fsck mkf2fs
   SUPPORTED_DEVICES += philips,hy3000
   KERNEL := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
   KERNEL_INITRAMFS := kernel-bin | lzma | \
